60d3f6faa4ddf3c1441cdcdb6101777266098fa1
DarkSuicune <ckasslertaub@pclcnj.com>
2014-11-28 12:41:50 -0500

    Update cards.js

--- chat-plugins/cards.js
+++ chat-plugins/cards.js
@@ -7,4 +7,8 @@ cangel: function(target, room, user) {
 if (!this.canBroadcast()) return;
 this.sendReplyBox("<font size=\"3\"> <center> <u> <b> CorruptedAngel </b> </u> </center> </font> <p> <center> <i> \"Son of a Demon?\" </i> </center> </p> <p> <center> <b> Ace: </b> Gengar <br /> <img src='http://static.tumblr.com/72b0e488f7368b1a830561752a2f630d/v6i8nev/Lpcn7inua/tumblr_static_ad4jqyfwk000kw8wo0480s0gs.gif' width=\"125\" height=\"125\ />");
 },
+bhope: function(target, room, user) {
+if (!this.canBroadcast()) return;
+this.sendReplyBox(<center><font color="#697AFA" size="3" face="BrokenGhost"><u><b>Broken Hope</b></u></center></font><p><center><i> \"I see now that the circumstances of one's birth are irrelevant. It is what you do with the gift of life that determines who you are"\" </i> </center> </p> <p> <center> <b> Ace: </b> Mewtwo <br /> <img src='http://www.pkparaiso.com/imagenes/xy/sprites/animados/mewtwo.gif' width=\"120\" height=\"106\ />");
+},
 };
