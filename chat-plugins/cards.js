99cc5e0f7e86f166e6b3a282a8b151714d0a7ff8
Joshua <cookcook54@yahoo.com>
2014-11-28 11:27:12 -0600

    Create cards.js

---
+++ chat-plugins/cards.js
@@ -0,0 +1,10 @@
+exports.commands = {
+sglaceon: function(target, room, user) {
+if (!this.canBroadcast()) return;
+this.sendReplyBox("<font size=\"3\"> <center> <u> <b> Shadowglaceon </b> </u> </center> </font> <p> <center> <i> \"This is my quote\" </i> </center> </p> <p> <center> <b> Ace: </b> Glaceon <br /> <img src='http://play.pokemonshowdown.com/sprites/xyani/glaceon' width=\"125\" height=\"125\ />");
+},
+cangel: function(target, room, user) {
+if (!this.canBroadcast()) return;
+this.sendReplyBox("<font size=\"3\"> <center> <u> <b> CorruptedAngel </b> </u> </center> </font> <p> <center> <i> \"Son of a Demon?\" </i> </center> </p> <p> <center> <b> Ace: </b> Gengar <br /> <img src='http://static.tumblr.com/72b0e488f7368b1a830561752a2f630d/v6i8nev/Lpcn7inua/tumblr_static_ad4jqyfwk000kw8wo0480s0gs.gif' width=\"125\" height=\"125\ />");
+},
+};
